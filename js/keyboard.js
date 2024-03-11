290867a4be7f3043b0a04195216a2a1f24ebfd0b
Vyachaslau <sokol.v083@gmail.com>
2024-03-11 16:12:34 +0300

    paddle movement

---
+++ js/keyboard.js
@@ -0,0 +1,5 @@
+// let left = false
+// let right = false
+
+// document.addEventListener("keydown", keyDownHandler, false);
+// document.addEventListener("keyup", keyUpHandler, false);
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { Canvas } from './Canvas.js';
 import { Ball } from './Ball.js';
+import './keyboard.js'
 
 const RADIUS = 10;
 let canvasWrapper = document.getElementById('canvas-wrapper');
@@ -9,23 +10,32 @@ canvasWrapper.style.width = `${window}px`;
 canvasWrapper.style.height = `${height}px`;
 const canvas = new Canvas(canvasWrapper);
 
-let paddleWidth = 100
-let paddleHeight = 10
-// console.log(height, paddleHeight)
+const paddleWidth = 100
+const paddleHeight = 10
+let paddleX = (width - paddleWidth) / 2
 
 let x = ~~(width / 2);
-let y = height - paddleHeight- RADIUS;
-console.log(x, y)
+let y = height - paddleHeight - RADIUS;
 const ball = new Ball(x, y, RADIUS, 2, -2)
 
 canvas.drawBall(ball);
-canvas.drawPaddle(x - paddleWidth/2, height - paddleHeight, paddleWidth, paddleHeight);
+canvas.drawPaddle(paddleX, height - paddleHeight, paddleWidth, paddleHeight);
+
+let leftPressed = false;
+let rightPressed = false;
 
 function draw() {
     canvas.clear()
     ball.update(canvas.ctx.width, canvas.ctx.height)
     canvas.drawBall(ball);
-    canvas.drawPaddle(x - paddleWidth/2, height - paddleHeight, paddleWidth, paddleHeight);
+
+    if (rightPressed) {
+        paddleX = Math.min(width - paddleWidth/2, paddleX + 6);
+    } else if (leftPressed) {
+        paddleX = Math.max(0, paddleX - 6);
+    }
+
+    canvas.drawPaddle(paddleX, height - paddleHeight, paddleWidth, paddleHeight);
 }
 
 function startGame() {
@@ -35,3 +45,16 @@ function startGame() {
 canvasWrapper.addEventListener('click', () => {
     startGame();
 })
+
+let keyDownHandler = function (e) {
+    if (e.key === "Right" || e.key === "ArrowRight" || e.key === "d") rightPressed = true
+    else if (e.key === "Left" || e.key === "ArrowLeft" || e.key === "a") leftPressed = true
+}
+
+let keyUpHandler = function (e) {
+    if (e.key === "Right" || e.key === "ArrowRight" || e.key === "d") rightPressed = false
+    else if (e.key === "Left" || e.key === "ArrowLeft" || e.key === "a") leftPressed = false
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
