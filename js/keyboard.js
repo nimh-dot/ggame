1a94104704ee9f320e5d9703b05a31c5b46b9f82
Vyachaslau <sokol.v083@gmail.com>
2024-03-13 18:11:35 +0300

    keyboard handlers into a module

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,5 +1,13 @@
-// let left = false
-// let right = false
+export let keyDownHandler = function (e, horizontal) {
+    if (e.key === "Right" || e.key === "ArrowRight" || e.key === "d") horizontal.right = true
+    else if (e.key === "Left" || e.key === "ArrowLeft" || e.key === "a") horizontal.left = true
 
-// document.addEventListener("keydown", keyDownHandler, false);
-// document.addEventListener("keyup", keyUpHandler, false);
+    return horizontal
+}
+
+export let keyUpHandler = function (e, horizontal) {
+    if (e.key === "Right" || e.key === "ArrowRight" || e.key === "d") horizontal.right = false
+    else if (e.key === "Left" || e.key === "ArrowLeft" || e.key === "a") horizontal.left = false
+
+    return horizontal
+}
--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import { Canvas } from './Canvas.js';
 import { Ball } from './Ball.js';
-import './keyboard.js'
+import { keyDownHandler, keyUpHandler } from './keyboard.js'
 
 const RADIUS = 10;
 let canvasWrapper = document.getElementById('canvas-wrapper');
 let width = window.innerWidth * 0.9
 let height = window.innerHeight * 0.7
-canvasWrapper.style.width = `${window}px`;
+canvasWrapper.style.width = `${width}px`;
 canvasWrapper.style.height = `${height}px`;
 const canvas = new Canvas(canvasWrapper);
 
@@ -21,17 +21,16 @@ const ball = new Ball(x, y, RADIUS, 2, -2)
 canvas.drawBall(ball);
 canvas.drawPaddle(paddleX, height - paddleHeight, paddleWidth, paddleHeight);
 
-let leftPressed = false;
-let rightPressed = false;
+let horizontal = { left: false, right: false }
 
 function draw() {
     canvas.clear()
     ball.update(canvas.ctx.width, canvas.ctx.height)
     canvas.drawBall(ball);
 
-    if (rightPressed) {
-        paddleX = Math.min(width - paddleWidth/2, paddleX + 6);
-    } else if (leftPressed) {
+    if (horizontal.right) {
+        paddleX = Math.min(width - paddleWidth, paddleX + 6);
+    } else if (horizontal.left) {
         paddleX = Math.max(0, paddleX - 6);
     }
 
@@ -42,19 +41,7 @@ function startGame() {
     const interval = setInterval(draw, 10);
 }
 
-canvasWrapper.addEventListener('click', () => {
-    startGame();
-})
+document.addEventListener("keydown", (e) => { horizontal = keyDownHandler(e, horizontal) }, false);
+document.addEventListener("keyup", (e) => { horizontal = keyUpHandler(e, horizontal) }, false);
 
-let keyDownHandler = function (e) {
-    if (e.key === "Right" || e.key === "ArrowRight" || e.key === "d") rightPressed = true
-    else if (e.key === "Left" || e.key === "ArrowLeft" || e.key === "a") leftPressed = true
-}
-
-let keyUpHandler = function (e) {
-    if (e.key === "Right" || e.key === "ArrowRight" || e.key === "d") rightPressed = false
-    else if (e.key === "Left" || e.key === "ArrowLeft" || e.key === "a") leftPressed = false
-}
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+canvasWrapper.addEventListener('click', () => startGame());
